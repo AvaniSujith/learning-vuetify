77c1afeaa8f25c58e723d4f6934278ceba29b6b9
AvaniSujith <chinchuavani123@gmail.com>
2025-07-11 12:25:35 +0530

    feat: vuetify cards

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -15,35 +15,45 @@ const rules = [
 ];
 
 const submit = () => {
-    alert(JSON.stringify(form.value))
-}
+  alert(JSON.stringify(form.value));
+};
 </script>
 
 <template>
-  <v-sheet class="mx-auto" width="300">
-    <v-form @submit.prevent="submit">
-      <v-text-field
-        v-model="form.email"
-        variant="solo"
-        prepend-inner-icon="mdi-mail"
-        :rules="rules"
-        label="Name"
-      ></v-text-field>
-      <v-text-field
-        v-model="form.password"
-        type="password"
-        variant="solo"
-        prepend-inner-icon="mdi-key"
-        label="Password"
-      >
-      </v-text-field>
-      <v-checkbox
-        v-model="form.remember"
-        label="Remember Me"
-        color="#000"
-        hide-details
-      ></v-checkbox>
-      <v-btn color="red-darken-3" class="mt-2" type="submit" block>Submit</v-btn>
-    </v-form>
-  </v-sheet>
+  <v-card class="pa-6 mx-auto" width="300">
+    <v-card-title class="text-center">Login Here</v-card-title>
+    <v-card-item>
+      <v-form @submit.prevent="submit">
+        <v-text-field
+          v-model="form.email"
+          variant="solo"
+          prepend-inner-icon="mdi-mail"
+          :rules="rules"
+          label="Name"
+        ></v-text-field>
+        <v-text-field
+          v-model="form.password"
+          type="password"
+          variant="solo"
+          prepend-inner-icon="mdi-key"
+          label="Password"
+        >
+        </v-text-field>
+        <v-checkbox
+          v-model="form.remember"
+          label="Remember Me"
+          color="#000"
+          hide-details
+        ></v-checkbox>
+        <v-btn color="red-darken-3" class="mt-2" type="submit" block
+          >Submit</v-btn
+        >
+      </v-form>
+    </v-card-item>
+    <v-card-action>
+      <div class="mx-4">
+        <v-btn block to="/register"> Register </v-btn>
+      </div>
+    </v-card-action>
+  </v-card>
 </template>
