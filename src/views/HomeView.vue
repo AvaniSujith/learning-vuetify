a8ebe154041296ce2976f1cace85e096474d1351
AvaniSujith <chinchuavani123@gmail.com>
2025-07-13 23:06:01 +0530

    feat: input bar attempt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+    label:string,
+    placeholder?: string,
+    modelValue: string | number,
 
+}>();
+</script>
 <template>
-  <div>Hello home</div>
+  <div>
+    <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+    <v-text-field
+      density="compact"
+      placeholder="Email address"
+      variant="outlined"
+    ></v-text-field>
+  </div>
 </template>
+<style scoped>
+
+</style> 
