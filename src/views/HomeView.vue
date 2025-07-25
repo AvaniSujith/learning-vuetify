3585721d303f532a28cd71ef8c6d3364aacc11f9
AvaniSujith <chinchuavani123@gmail.com>
2025-07-25 23:45:54 +0530

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
-const props = defineProps<{
-    label:string,
-    placeholder?: string,
-    modelValue: string | number,
+interface Props {
+  
+  label: string;
+  modelValue: string | number;
+  required: boolean;
+  disabled?: boolean;
+}
 
+const props = withDefaults(defineProps<Props>(), {
+  disabled: false,
+});
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
 }>();
+
 </script>
 <template>
   <div>
     <div class="text-subtitle-1 text-medium-emphasis">Account</div>
     <v-text-field
+      v-model="modelValue"
       density="compact"
-      placeholder="Email address"
+      type="text"
       variant="outlined"
+      :disabled="props.disabled"
+      :required="true"
+      hide-details="auto"
     ></v-text-field>
   </div>
 </template>
-<style scoped>
 
-</style> 
+<style scoped></style>
